adcf1021fee82665f894a8af0f2fe5ee015a9469
Lukas Winkler <git@lw1.at>
2021-09-07 16:07:13 +0200

    set ALLOWED_HOSTS

--- rpg_notes/settings.py
+++ rpg_notes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".rpgnotes.lw1.at"]
 
 # Application definition
 
