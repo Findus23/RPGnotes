462c59314bd5ce74f874b012c85acf7baea99d9c
Lukas Winkler <git@lw1.at>
2021-10-15 22:05:11 +0200

    improve jinja templates

--- common/templatetags/bootstrap5.py
+++ common/templatetags/bootstrap5.py
@@ -5,3 +5,13 @@ from django_jinja import library
 @library.global_function
 def bootstrap_form(*args, **kwargs):
     return django_bootstrap5.bootstrap_form(*args, **kwargs)
+
+
+@library.global_function
+def bootstrap_alert(*args, **kwargs):
+    return django_bootstrap5.bootstrap_alert(*args, **kwargs)
+
+
+@library.filter
+def bootstrap_message_alert_type(*args, **kwargs):
+    return django_bootstrap5.bootstrap_message_alert_type(*args, **kwargs)
