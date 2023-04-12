edf46c59b105299b352c5be126fded4309fa2843
Lukas Winkler <git@lw1.at>
2023-04-12 22:38:30 +0200

    update dependencies

--- common/templatetags/version.py
+++ common/templatetags/version.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from pathlib import Path
 from subprocess import run
 
 from django.core.cache import cache
@@ -8,9 +9,10 @@ from django_jinja import library
 @library.global_function
 @lru_cache(maxsize=None)
 def commit_id():
+    current_dir = Path(__file__).parent
     commit = cache.get("commit")
     if not commit:
-        sp = run(["git", "rev-parse", "--verify", "HEAD"], capture_output=True)
+        sp = run(["git", "rev-parse", "--verify", "HEAD"], capture_output=True, cwd=current_dir)
         commit = sp.stdout.decode().strip()
         cache.set("commit", commit)
     return commit
