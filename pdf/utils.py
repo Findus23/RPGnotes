2086c7df9d17bd5edafa5ece998737c623ec1f3d
Lukas Winkler <git@lw1.at>
2024-08-07 23:36:35 +0200

    proper raw strings

--- pdf/utils.py
+++ pdf/utils.py
@@ -38,11 +38,11 @@ def md_to_latex(text: str) -> str:
 
 
 env = jinja2.Environment(
-    block_start_string='\BLOCK{',
+    block_start_string=r'\BLOCK{',
     block_end_string='}',
-    variable_start_string='\VAR{',
+    variable_start_string=r'\VAR{',
     variable_end_string='}',
-    comment_start_string='\#{',
+    comment_start_string=r'\#{',
     comment_end_string='}',
     line_statement_prefix='%#',
     line_comment_prefix='%%',
